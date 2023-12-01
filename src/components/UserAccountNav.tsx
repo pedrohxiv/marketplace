b2967a031082483f6dce75a6d2393a859b03d36e
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2023-12-01 15:11:29 -0300

    Initial commit

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAuth } from "@/hooks/use-auth";
+import { User } from "@/payload-types";
+
+interface UserAccountNavProps {
+  user: User;
+}
+
+export const UserAccountNav = ({ user }: UserAccountNavProps) => {
+  const { signOut } = useAuth();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="overflow-visible">
+        <Button variant="ghost" size="sm" className="relative">
+          My account
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white w-72" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            <p className="font-medium text-sm text-black">{user.email}</p>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/sell">Seller Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={signOut} className="cursor-pointer">
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
